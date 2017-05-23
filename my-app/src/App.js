13f11d50f7dd45e8fe97872cc34bb796c03ffa17
sarahfeldmann <sarahf95@gmail.com>
2017-05-22 17:06:09 -0700

    add cursor

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Input from './input';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
 
-        <div className="rectangle"></div>
+        <div className="rectangle">
+  <Input/>
 
+
+        </div>
+      
       </div>
     );
   }
---
+++ my-app/src/input.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Input extends Component {
+  render() {
+    return (
+      <div className="Input">
+
+        <div className="input"> <span className="blinking-cursor">|</span></div>
+
+      </div>
+    );
+  }
+}
+
+export default Input;
