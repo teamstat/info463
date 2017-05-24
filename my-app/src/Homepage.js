0fa6391211a29b47baa584d5282bbf5c239bf7bc
sarahfeldmann <sarahf95@gmail.com>
2017-05-23 22:53:26 -0700

    add broken routing

--- my-app/src/Homepage.js
+++ my-app/src/Homepage.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router';
 
 
 class Homepage extends Component {
+
+
     render() {
         return (
             <div className="homepage">
-
-                <div className="g-m">G-M</div>
+                <Link to="/g-m"><div className="g-m">G-M</div></Link>
                 <div className="middle">
-                    <span className="a-f">A-F</span>
+                   <Link to="/a-f"> <span className="a-f">A-F</span></Link>
                     <span className="space">space</span>
-                    <span className="n-t">N-T</span>
+                   <Link to="/n-t"> <span className="n-t">N-T</span></Link>
                 </div>
-                <div className="u-z">U-Z</div>
-
-
+                <Link to="/u-z"><div className="u-z">U-Z</div></Link>
             </div>
         );
     }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,8 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import Homepage from './Homepage'
+import AF from './AF';
+import GM from './GM';
+import NT from './NT';
+import UZ from './UZ';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+
 
 ReactDOM.render(
-  <App />,
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Homepage} />
+      <Route path="a-f" component={AF} />
+      <Route path="g-m" component={GM} />
+      <Route path="n-t" component={NT} />
+      <Route path="u-z" component={UZ} />
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
